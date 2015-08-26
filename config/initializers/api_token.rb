1b71e8c526534f53baec8f4706b4632b6c7fac09
Rada Bogdan <raoul_ice@yahoo.com>
2015-08-26 15:12:25 +0300

    fix namespace

--- config/initializers/api_token.rb
+++ config/initializers/api_token.rb
@@ -1,2 +1,7 @@
   TestApp::Application.config.api_auth_token  = 'iyFGGfLz1mIjegs6PZ5YkbIYXgNfkeWAR1xqoeZUNKNgFsWxBsoAfLTSXcy42iS3spE6fvvveafR98Iza3bKttK8L8zMvbkylemO'
+
+  if Rails.env.development?
   TestApp::Application.config.api_namespace = "http://localhost:3000/api/"
+else
+    TestApp::Application.config.api_namespace = "https://washout-builder.herokuapp.com/api/"
+end
