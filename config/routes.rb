10787093ebb6d9f9da1212e933346f403adf57db
Rada Bogdan <raoul_ice@yahoo.com>
2016-06-17 15:05:31 +0300

    mount the WashoutBuilder somewhere else to test both cases when acessing with /soap_doc and when using the mounting point

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+    root :to => WashoutBuilder::Engine
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   namespace :api do
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   mount MyEngine::Engine => '/engine1'
   mount Namespace::MyEngine2::Engine => '/engine2'
 
-  mount WashoutBuilder::Engine => '/'
+  mount WashoutBuilder::Engine => '/washout'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
